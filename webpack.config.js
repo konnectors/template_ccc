c0e21036520ec763bd05b91c0d39dfb1a7103cac
doubleface <christophe@cozycloud.cc>
2023-01-25 18:17:19 +0100

    feat: copy manifest and assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
-  mode: 'none'
+  mode: 'none',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'manifest.konnector' }, { from: 'assets' }]
+    })
+  ]
 }
