ec1490d936e473d2eecbe959c31c042f940b61c3
doubleface <christophe@cozycloud.cc>
2023-01-25 18:07:53 +0100

    fix: No more need of hook shell script
    The now needs to be a pure js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,3 @@
-const HookShellScriptPlugin = require('hook-shell-script-webpack-plugin')
 module.exports = {
-  mode: 'none',
-  plugins: [
-    new HookShellScriptPlugin({
-      afterCompile: [
-        `node ./scripts/generateWebViewContentScript.js ${__dirname}`
-      ]
-    })
-  ]
+  mode: 'none'
 }
