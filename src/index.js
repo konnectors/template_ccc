de069767fd60f0fbff3dc66c245d543f4b429a74
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2025-04-23 16:56:12 +0100

    Clean template's code
    We begin with the cleaning of the template's code to make place for your own.
    You can see we're keeping some mandatory functions. They will be overriden as they will be launched by the pilot as the path to follow during konnector's execution.
    We're also renaming the Class to fit targeted website.

--- src/index.js
+++ src/index.js
@@ -1,92 +1,25 @@
 import { ContentScript } from 'cozy-clisk/dist/contentscript'
 import Minilog from '@cozy/minilog'
 const log = Minilog('ContentScript')
-Minilog.enable()
+Minilog.enable('BookToScrapeCCC')
 
-const baseUrl = 'https://toscrape.com'
-const defaultSelector = "a[href='http://quotes.toscrape.com']"
-const loginLinkSelector = `[href='/login']`
-const logoutLinkSelector = `[href='/logout']`
-
-class TemplateContentScript extends ContentScript {
-  async navigateToLoginForm() {
-    this.log('info', '🤖 navigateToLoginForm')
-    await this.goto(baseUrl)
-    await this.waitForElementInWorker(defaultSelector)
-    await this.runInWorker('click', defaultSelector)
-    // wait for both logout or login link to be sure to check authentication when ready
-    await Promise.race([
-      this.waitForElementInWorker(loginLinkSelector),
-      this.waitForElementInWorker(logoutLinkSelector)
-    ])
+class BookToScrapeContentScript extends ContentScript {
+  onWorkerReady() {
   }
 
   onWorkerEvent({ event, payload }) {
-    if (event === 'loginSubmit') {
-      this.log('info', 'received loginSubmit, blocking user interactions')
-      this.blockWorkerInteractions()
-    } else if (event === 'loginError') {
-      this.log(
-        'info',
-        'received loginError, unblocking user interactions: ' + payload?.msg
-      )
-      this.unblockWorkerInteractions()
-    }
   }
 
   async ensureAuthenticated({ account }) {
-    this.bridge.addEventListener('workerEvent', this.onWorkerEvent.bind(this))
     this.log('info', '🤖 ensureAuthenticated')
-    if (!account) {
-      await this.ensureNotAuthenticated()
-    }
-    await this.navigateToLoginForm()
-    const authenticated = await this.runInWorker('checkAuthenticated')
-    if (!authenticated) {
-      this.log('info', 'Not authenticated')
-      await this.showLoginFormAndWaitForAuthentication()
-    }
-    this.unblockWorkerInteractions()
-    return true
   }
 
   async ensureNotAuthenticated() {
     this.log('info', '🤖 ensureNotAuthenticated')
-    await this.navigateToLoginForm()
-    const authenticated = await this.runInWorker('checkAuthenticated')
-    if (!authenticated) {
-      return true
-    }
-
-    await this.clickAndWait(logoutLinkSelector, loginLinkSelector)
-    return true
-  }
-
-  onWorkerReady() {
-    window.addEventListener('DOMContentLoaded', () => {
-      const button = document.querySelector('input[type=submit]')
-      if (button) {
-        button.addEventListener('click', () =>
-          this.bridge.emit('workerEvent', { event: 'loginSubmit' })
-        )
-      }
-      const error = document.querySelector('.error')
-      if (error) {
-        this.bridge.emit('workerEvent', {
-          event: 'loginError',
-          payload: { msg: error.innerHTML }
-        })
-      }
-    })
-  }
-
-  async checkAuthenticated() {
-    return Boolean(document.querySelector(logoutLinkSelector))
   }
 
   async showLoginFormAndWaitForAuthentication() {
-    log.debug('showLoginFormAndWaitForAuthentication start')
-    await this.clickAndWait(loginLinkSelector, '#username')
+    this.log('info', '🤖 showLoginFormAndWaitForAuthentication')
     await this.setWorkerState({ visible: true })
     await this.runInWorkerUntilTrue({
       method: 'waitForAuthenticated'
@@ -94,70 +27,21 @@ class TemplateContentScript extends ContentScript {
     await this.setWorkerState({ visible: false })
   }
 
-  async fetch(context) {
-    this.log('info', '🤖 fetch')
-    await this.goto('https://books.toscrape.com')
-    await this.waitForElementInWorker('#promotions')
-    const bills = await this.runInWorker('parseBills')
-
-    await this.saveBills(bills, {
-      contentType: 'image/jpeg',
-      fileIdAttributes: ['filename'],
-      context
-    })
-
-    const identity = await this.runInWorker('parseIdentity')
-    await this.saveIdentity(identity)
-  }
+  async checkAuthenticated() {}
 
   async getUserDataFromWebsite() {
     this.log('info', '🤖 getUserDataFromWebsite')
-    return {
-      sourceAccountIdentifier: 'defaultTemplateSourceAccountIdentifier'
-    }
   }
 
-  async parseBills() {
-    const articles = document.querySelectorAll('article')
-    return Array.from(articles).map(article => ({
-      amount: normalizePrice(article.querySelector('.price_color')?.innerHTML),
-      date: '2024-01-01', // use a fixed date to avoid the multiplication of bills
-      vendor: 'template',
-      filename: article.querySelector('h3 a')?.getAttribute('title'),
-      fileurl:
-        'https://books.toscrape.com/' +
-        article.querySelector('img')?.getAttribute('src')
-    }))
+  async fetch(context) {
+    this.log('info', '🤖 fetch')
   }
 
-  async parseIdentity() {
-    const contact = {
-      name: {
-        givenName: 'John',
-        familyName: 'Doe'
-      },
-      address: [
-        {
-          street: '2 rue du moulin',
-          postcode: '00000',
-          city: 'Paris',
-          formattedAddress: '2 rue du moulin 00000 Paris'
-        }
-      ],
-      email: [{ address: 'mail@mail.com' }]
-    }
-    return { contact }
-  }
 }
 
-// Convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
-}
-
-const connector = new TemplateContentScript()
+const connector = new BookToScrapeContentScript()
 connector
-  .init({ additionalExposedMethodsNames: ['parseBills', 'parseIdentity'] })
+  .init({ additionalExposedMethodsNames: [] })
   .catch(err => {
     log.warn(err)
   })
