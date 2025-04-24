74124b653ae8424891cff4283a0a8042700f48d3
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2025-04-24 16:14:11 +0100

    EnsureNotAuthenticated
    In this commit we are starting by ensuring there is no active session on the targeted website so we can begin on a fresh start. As it is (and will be until we create a cozy account and saved some credentials) the first ever konnector's run on the device.
    To cover for the automated logout too, we reach the "login successfully" page of the targeted website.
    When one or the other element is detected, we're launching `checkAuthenticated` in the worker to determine if the logout button is visible on the page.
    If it is not, here we go, we are ready to let the user login ! If it is, we proceed to click on the logout button and wait for the wanted form input element to show up, indicating the logout has been done properly.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import { ContentScript } from 'cozy-clisk/dist/contentscript'
 import Minilog from '@cozy/minilog'
 const log = Minilog('ContentScript')
-Minilog.enable('BookToScrapeCCC')
+Minilog.enable('BooksToScrapeCCC')
+
+// URLS
+
+// As mentionned in the documentation, booksToScrape does not have any login logic
+// To illustrate this part, we're gonne use a automation testing website that have one.
+const loginFormUrl = "https://practicetestautomation.com/practice-test-login/"
+const loginSuccessfullUrl = "https://practicetestautomation.com/logged-in-successfully/"
+const homePageUrl = "https://books.toscrape.com/"
+
+// ELEMENTS
+
+const connectedElementSelector = 'a[href="https://practicetestautomation.com/practice-test-login/"]'
+const usernameInputSelector = "#username"
 
 class BookToScrapeContentScript extends ContentScript {
   onWorkerReady() {
@@ -12,10 +25,27 @@ class BookToScrapeContentScript extends ContentScript {
 
   async ensureAuthenticated({ account }) {
     this.log('info', '🤖 ensureAuthenticated')
+    const credentials = await this.getCredentials()
+    if (!account || !credentials) {
+      await this.ensureNotAuthenticated()
+    }
   }
 
   async ensureNotAuthenticated() {
     this.log('info', '🤖 ensureNotAuthenticated')
+    // To test the path where no logout is required, just change the selector to loginFormUrl
+    await this.goto(loginSuccessfullUrl)
+    // Waiting for elements indicating we're connected or not
+    await this.waitForElementInWorker(`${usernameInputSelector}, ${connectedElementSelector}`)
+    const authenticated = await this.runInWorker('checkAuthenticated')
+    if(!authenticated){
+      this.log('info', 'ensureNotAuthenticated - User is already disconnected')
+      return true
+    }
+    await this.runInWorker('click', 'a[href="https://practicetestautomation.com/practice-test-login/"]')
+    await this.waitForElementInWorker(usernameInputSelector)
+    this.log('info', 'ensureNotAuthenticated - User has been disconnected')
+    return true
   }
 
   async showLoginFormAndWaitForAuthentication() {
@@ -27,7 +57,9 @@ class BookToScrapeContentScript extends ContentScript {
     await this.setWorkerState({ visible: false })
   }
 
-  async checkAuthenticated() {}
+  async checkAuthenticated() {
+    return Boolean(document.querySelector(connectedElementSelector))
+  }
 
   async getUserDataFromWebsite() {
     this.log('info', '🤖 getUserDataFromWebsite')
